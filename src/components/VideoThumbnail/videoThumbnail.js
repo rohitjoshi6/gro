4059875cef19020adfc56dc9c586e3fa455bda38
Rohit_Joshi <rohitjoshi632001@gmail.com>
2023-05-23 22:36:34 +0530

    component

---
+++ src/components/VideoThumbnail/videoThumbnail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+
+const videoThumbnail = ({video, onThumbnailClick}) => {
+  return (
+    <>
+      <h3>title</h3>
+    </>
+  )
+}
+
+export default videoThumbnail
---
+++ src/pages/LandingPage/landingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import VideoPlayer from '../../components/VideoPlayer/videoPlayer'
+import VideoThumbnail from '../../components/VideoThumbnail/videoThumbnail'
+
+const landingPage = () => {
+  return (
+    <div>
+        <VideoPlayer />
+        <VideoThumbnail />
+    </div>
+  )
+}
+
+export default landingPage
